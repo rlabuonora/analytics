7cd869dc5a3ef9cecdc45511d47b2a70f6e88b36
Rafa <rlabuonora@opp.gub.uy>
2017-10-02 17:04:59 -0300

    finish stock dynamics

--- unit1/stock_dynamics/stock_dynamics.R
+++ unit1/stock_dynamics/stock_dynamics.R
@@ -62,3 +62,80 @@ sd(ProcterGamble$StockPrice)
 
 # Using the plot function, plot the Date on the x-axis and 
 # the StockPrice on the y-axis, for Coca-Cola.
+
+# Using the plot function, plot the Date on the x-axis 
+# and the StockPrice on the y-axis, for Coca-Cola.
+
+plot(CocaCola$Date, CocaCola$StockPrice, type="l", col="red")
+
+# Around what year did Coca-Cola has its highest stock price in this time period?
+# 1973
+
+# Around what year did Coca-Cola has its lowest stock price in this time period?
+# 1980
+
+lines(ProcterGamble$Date, ProcterGamble$StockPrice, col="blue")
+
+# In March of 2000, the technology bubble burst, and a stock market crash occurred. 
+# According to this plot, which company's stock dropped more?
+# ProcterGamble
+
+# Around 1983, the stock for one of these companies 
+# (Coca-Cola or Procter and Gamble) was going up, 
+# while the other was going down. Which one was going up?
+
+abline(v=as.Date(c("1983-06-06")), lwd=2)
+# Procter and Gamble
+
+# In the time period shown in the plot, 
+# which stock generally has lower values?
+# Coca-Cola 
+
+
+plot(CocaCola$Date[301:432], CocaCola$StockPrice[301:432], type="l", col="red", ylim=c(0,210))
+lines(Boeing$Date[301:432], Boeing$StockPrice[301:432], col="green")
+lines(GE$Date[301:432], GE$StockPrice[301:432], col="blue")
+lines(IBM$Date[301:432], IBM$StockPrice[301:432], col="black")
+lines(ProcterGamble$Date[301:432], ProcterGamble$StockPrice[301:432], col="magenta")
+
+# Which stock fell the most 
+# right after the technology bubble burst in March 2000?
+# General Electric (GE) (blue)
+
+# Which stock reaches the highest value in the time period 1995-2005?
+# IBM
+
+
+# In October of 1997, there was a global stock market crash 
+# that was caused by an economic crisis in Asia. 
+# Comparing September 1997 to November 1997, 
+# which companies saw a decreasing trend in their stock price?
+
+abline(v=as.Date(c("1997-08-30")), lwd=1)
+
+# ProcterGamble (Magenta)
+# Boeing (Green)
+
+# In the last two years of this time period (2004 and 2005) 
+# which stock seems to be performing the best, 
+# in terms of increasing stock price?
+# Boeing
+
+
+# Lastly, let's see if stocks tend to be higher or lower during certain months. 
+# Use the tapply command to calculate the mean stock price of IBM, sorted by months. To sort by months, use
+# For IBM, compare the monthly averages to the overall average stock price. 
+# In which months has IBM historically had a higher stock price (on average)? 
+
+which(tapply(IBM$StockPrice, months(IBM$Date), mean) > mean(IBM$StockPrice))
+# April February  January    March      May 
+# 1        4        5        8        9 
+
+
+
+which.max(tapply(GE$StockPrice, months(GE$Date), mean))
+which.max(tapply(CocaCola$StockPrice, months(CocaCola$Date), mean))
+
+tapply(GE$StockPrice, months(GE$Date), mean)
+tapply(CocaCola$StockPrice, months(CocaCola$Date), mean)
+# December
